acb8ba0a184b8f8d9ec1637a6ff665c3a37b472d
Julian Camilo <julian20141016@gmail.com>
2021-06-03 07:58:53 -0500

    Creada credit card View con random CC generators

--- components/Configurations.jsx
+++ components/Configurations.jsx
@@ -1,8 +1,36 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
+import CreditCardGenerator from "creditcard-generator";
+import CreditCardDisplay from "react-native-credit-card-display";
+
+const getRandomCC = () => {
+  return CreditCardGenerator.GenCC();
+};
 
 const Configurations = () => {
-  return <Text>Pantalla del configurador</Text>;
+  return (
+    <View style={styles.containerPage}>
+      <CreditCardDisplay
+        number={getRandomCC()}
+        cvc={432}
+        expiration="07/24"
+        name="Julian Camilo Cruz"
+        since="2020"
+      />
+      <View>
+        <Text>Mira nomas que belleza de CC</Text>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  containerPage: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "20%",
+  },
+});
 
 export default Configurations;
