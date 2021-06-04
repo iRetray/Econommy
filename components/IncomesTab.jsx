0967b2df9d87c4569b7fe86acdc87aee52de940c
Brayan Cruz <>
2021-06-04 13:16:45 -0500

    Creado Tab Navigator para pantalla de Transacciones

---
+++ components/IncomesTab.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const IncomesTab = () => {
+  return (
+    <View>
+      <Text>Tab de ingresos</Text>
+    </View>
+  );
+};
+
+export default IncomesTab;
--- components/Transactions.jsx
+++ components/Transactions.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { SafeAreaView, ScrollView } from 'react-native';
-import { Button, Icon, List, ListItem } from '@ui-kitten/components';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, Icon, List, ListItem, TabBar } from '@ui-kitten/components';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createMaterialTopTabNavigator();
+
+import IncomesTab from './IncomesTab';
+import ExpensesTab from './ExpensesTab';
 
 const Transactions = () => {
+  const [selectedTab, setSelectedTab] = React.useState('INCOMES');
+
   const data = new Array(20).fill({
     title: 'Title for Item',
     description: 'Description for Item',
@@ -23,11 +32,49 @@ const Transactions = () => {
 
   return (
     <SafeAreaView>
+      <View style={styles.tabBar}>
+        <View style={styles.tab}>
+          <Icon style={styles.iconsTab} fill="#389e0d" name="flash" />
+          <Text style={styles.textIncomes}>Ingresos</Text>
+        </View>
+        <View style={styles.tab}>
+          <Icon style={styles.iconsTab} fill="#595959" name="briefcase" />
+          <Text style={styles.work}>Todo</Text>
+        </View>
+        <View style={{ ...styles.tab }}>
+          <Icon style={styles.iconsTab} fill="#595959" name="briefcase" />
+          <Text style={styles.work}>Gastos</Text>
+        </View>
+      </View>
+
+      <Text>El tab seleccionado es: {selectedTab}</Text>
       <ScrollView>
         <List data={data} renderItem={renderItem} />
       </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  tabBar: {
+    marginTop: '10%',
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+  },
+  tab: {
+    alignItems: 'center',
+    alignContent: 'center',
+    width: '33%',
+  },
+  iconsTab: {
+    width: 30,
+    height: 30,
+  },
+  textIncomes: {
+    color: '#389e0d',
+    fontWeight: 'bold',
+  },
+});
 
 export default Transactions;
